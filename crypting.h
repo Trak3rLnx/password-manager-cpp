e24d86b210009851b1880110ca3b8035487e899f
Trak3r <162452450+Trak3rLnx@users.noreply.github.com>
2025-03-11 15:35:42 +0100

    Update commentary

--- crypting.h
+++ crypting.h
@@ -9,7 +9,7 @@ inline std::string aesEncrypt(const std::string &plaintext,
                               const std::string &passphrase) {
   OpenSSL_add_all_algorithms();
 
-  // Générer une clé à partir de la passphrase
+  // on génère une clé à partir de la passphrase
   unsigned char key[EVP_MAX_KEY_LENGTH], iv[EVP_MAX_IV_LENGTH];
   if (EVP_BytesToKey(
           EVP_aes_256_cbc(), EVP_sha256(), nullptr,
@@ -18,19 +18,19 @@ inline std::string aesEncrypt(const std::string &plaintext,
     throw std::runtime_error("Error generating key");
   }
 
-  // Initialiser le contexte de chiffrement
+  // truc compliqué
   EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
   if (!ctx) {
     throw std::runtime_error("Error creating cipher context");
   }
 
-  // Initialiser le chiffrement
+  // on initialise le chiffrement avec l'iv et la clé
   if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv) != 1) {
     EVP_CIPHER_CTX_free(ctx);
     throw std::runtime_error("Error initializing encryption");
   }
 
-  // Chiffrement du texte
+  // on chiffre le texte
   std::string ciphertext;
   int len;
   unsigned char buffer[4096];
@@ -43,17 +43,16 @@ inline std::string aesEncrypt(const std::string &plaintext,
   }
   ciphertext.append(reinterpret_cast<char *>(buffer), len);
 
-  // Finaliser le chiffrement
+  // fin du chiffrement
   if (EVP_EncryptFinal_ex(ctx, buffer, &len) != 1) {
     EVP_CIPHER_CTX_free(ctx);
     throw std::runtime_error("Bad passphrase !");
   }
   ciphertext.append(reinterpret_cast<char *>(buffer), len);
 
-  // Libérer le contexte de chiffrement
+  // truc compliqué encore
   EVP_CIPHER_CTX_free(ctx);
 
-  // Retourner le texte chiffré
   return ciphertext;
 }
 
@@ -61,7 +60,7 @@ inline std::string aesDecrypt(const std::string &ciphertext,
                               const std::string &passphrase) {
   OpenSSL_add_all_algorithms();
 
-  // Générer une clé à partir de la passphrase
+  // generation d'une clé à partir de la passphrase
   unsigned char key[EVP_MAX_KEY_LENGTH], iv[EVP_MAX_IV_LENGTH];
   if (EVP_BytesToKey(
           EVP_aes_256_cbc(), EVP_sha256(), nullptr,
@@ -70,19 +69,19 @@ inline std::string aesDecrypt(const std::string &ciphertext,
     throw std::runtime_error("Error generating key");
   }
 
-  // Initialiser le contexte de déchiffrement
+  // truc compliqué encore et encore...
   EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
   if (!ctx) {
     throw std::runtime_error("Error creating cipher context");
   }
 
-  // Initialiser le déchiffrement
+  // initialisation du déchiffrement avec la clé et l'IV
   if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv) != 1) {
     EVP_CIPHER_CTX_free(ctx);
     throw std::runtime_error("Error initializing decryption");
   }
 
-  // Déchiffrement du texte
+  // on dechiffre
   std::string plaintext;
   int len;
   unsigned char buffer[4096];
@@ -95,16 +94,15 @@ inline std::string aesDecrypt(const std::string &ciphertext,
   }
   plaintext.append(reinterpret_cast<char *>(buffer), len);
 
-  // Finaliser le déchiffrement
+  // fin du déchiffrement
   if (EVP_DecryptFinal_ex(ctx, buffer, &len) != 1) {
     EVP_CIPHER_CTX_free(ctx);
     throw std::runtime_error("Error ending decrypting ! ");
   }
   plaintext.append(reinterpret_cast<char *>(buffer), len);
 
-  // Libérer le contexte de déchiffrement
+  // truc compliqué... et bah oui encore j'y peux rien moi
   EVP_CIPHER_CTX_free(ctx);
 
-  // Retourner le texte déchiffré
   return plaintext;
 }
